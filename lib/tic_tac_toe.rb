953bbc26d6b4b0b84115f716aef078376da5a1bd
Beau <beaulazear@gmail.com>
2023-05-16 23:03:02 -0400

    commit tree 52c48d01f9a8944dd8f84566cd5208c0ab0087d8

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,27 @@
+require 'pry';
+
+class TicTacToe
+    def initialize 
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+
+    def display_board
+        # @board.each {|x| print x}
+        print @board
+    end
+
+    def input_to_index (string)
+        @board[string.to_i - 1]
+    end
+end
